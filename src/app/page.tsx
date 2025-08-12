c963e8cadd5a87a891196a64d60328ccc8c86d5a
Qunitee <lomak337@gmail.com>
2025-08-12 19:30:09 +0300

    added button and new page to stepper

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import FirstPage from './shared/first-page/first-page';
-// TODO delete this test page to change switching mode and check how ui library works later
+
 export default function MainPage() {
   return (
     <>
--- src/app/shared/first-page/first-page.tsx
+++ src/app/shared/first-page/first-page.tsx
@@ -5,15 +5,23 @@ import { StepperPanel } from 'primereact/stepperpanel';
 import {useRef} from 'react';
 import './styles/first-page.scss'
 import FirstForm from './first-form'
+import PageButton from '../../../shared/сomponents/page-button/page-button';
 
 export default function FirstPage() {
   const stepperRef = useRef(null);
   return (
     <div className="flex items-center">
       <Stepper className={'MyStepper'} ref={stepperRef} style={{ flexBasis: '60rem' }}>
-        <StepperPanel header="Header I">
+        <StepperPanel header="Page I">
           <div className=" border 1px flex items-center justify-center min-h-[60vh] ">
             <div className="flex-auto flex justify-center align-items-center font-medium"><FirstForm/></div>
+            <PageButton onClick={() => stepperRef.current.nextCallback()}/>
+          </div>
+        </StepperPanel>
+        <StepperPanel header="Page 2">
+          <div className=" border 1px flex items-center justify-center ">
+            <div className="flex-auto flex justify-center align-items-center font-medium"><FirstForm/></div>
+            <PageButton className='scale-x-[-1]' onClick={() => stepperRef.current.prevCallback()}/>
           </div>
         </StepperPanel>
       </Stepper>
