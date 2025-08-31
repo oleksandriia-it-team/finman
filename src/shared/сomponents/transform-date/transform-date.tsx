0b1b1e469043da3e447cdb4d144e8df3e2965690
Toha <reapersansaut@gmail.com>
2025-08-31 23:27:46 +0300

    Refactor date formatting logic to use PascalCase for enum values and update import paths

--- src/shared/сomponents/transform-date/transform-date.tsx
+++ src/shared/сomponents/transform-date/transform-date.tsx
@@ -1,7 +1,7 @@
-import { FormatDate } from '../../../data-access/date-service/date.service';
 import React, { useMemo } from 'react';
 import { TransformDateProps } from '../models/transform-date.model';
 import { defaultLocale } from '../../utils/get-preferred-locale.util';
+import { FormatDate } from '../../utils/format-date.util';
 
 export default function TransformDate({ date, type, locale = defaultLocale }: TransformDateProps) {
   const formatted = useMemo(() => {
