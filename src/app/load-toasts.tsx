7adf98794b3b9e176f80faa1a6aaa851b991bcdd
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-01-26 00:53:25 +0200

    added animation

--- src/app/load-toasts.tsx
+++ src/app/load-toasts.tsx
@@ -1,11 +1,15 @@
 import { useToastHook } from '../client/shared/hooks/toast/toast.hook';
 import ToastTemplate from '../client/shared/сomponents/toast/toast-template';
+import { useAutoAnimate } from '@formkit/auto-animate/react';
 
 export default function LoadToasts() {
   const toasts = useToastHook((state) => state.toasts);
 
+  const [parent] = useAutoAnimate();
+
   return (
     <div
+      ref={parent}
       className="toast-container position-fixed bottom-0 end-0 p-3"
       style={{ zIndex: 1055 }}
     >
