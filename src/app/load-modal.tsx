3f3f1adc1fd8b10983ad6ad6739d9971e6fab123
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-02-03 19:10:33 +0200

    added animation for modal

--- src/app/load-modal.tsx
+++ src/app/load-modal.tsx
@@ -2,6 +2,8 @@ import { useModalStore } from '../client/shared/hooks/modal/modal.hook';
 import { useShallow } from 'zustand/react/shallow';
 import { useEffect, useState } from 'react';
 import { useEventKey } from '../client/shared/hooks/event-key/event-key-hook';
+import { useBootstrapAnimation } from '../client/shared/hooks/bootstrap-animation/bootstrap-animation.hook';
+import clsx from 'clsx';
 
 export default function LoadModal() {
   const { isOpen, template, hideModal, onClose } = useModalStore(
@@ -26,22 +28,35 @@ export default function LoadModal() {
     hideModal();
   });
 
-  if (!isOpen || !template) return null;
+  const {
+    shouldRender,
+    showElement,
+    animatedData: [templateToRender],
+  } = useBootstrapAnimation(isOpen, 300, template);
+
+  if (!shouldRender || !templateToRender) return null;
+
+  const wrapperModalClasses = clsx('modal', showElement && 'show', 'd-block', 'fade');
+  const backdropClasses = clsx('modal-backdrop', 'fade', showElement && 'show');
 
   return (
-    <div
-      className="modal show d-block"
-      ref={setModalRef}
-      onClick={(event) => {
-        if (event.target === modalRef) {
-          onClose?.();
-          hideModal();
-        }
-      }}
-      tabIndex={-1}
-      role="dialog"
-    >
-      {template}
-    </div>
+    <>
+      <div
+        className={wrapperModalClasses}
+        ref={setModalRef}
+        onClick={(event) => {
+          if (event.target === modalRef) {
+            onClose?.();
+            hideModal();
+          }
+        }}
+        tabIndex={-1}
+        role="dialog"
+        aria-modal="true"
+      >
+        {templateToRender}
+      </div>
+      <div className={backdropClasses} />
+    </>
   );
 }
