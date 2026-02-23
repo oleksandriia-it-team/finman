2e9a199559d3e8fae40de6dcbc98326d829ee93a
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-02-23 15:47:50 +0200

    removed languages

--- src/app/api/lookups/languages/shared/models/language-payloads.model.ts
+++
@@ -1,6 +0,0 @@
-import { LanguagesSchema } from '../schemas/languages.schema';
-import { z } from 'zod';
-
-export type GetByLanguagePayload = z.infer<(typeof LanguagesSchema)['getByIdSchema']>;
-export type GetTotalLanguagesPayload = z.infer<(typeof LanguagesSchema)['totalCountSchema']>;
-export type GetLanguagesPayload = z.infer<(typeof LanguagesSchema)['itemsSchema']>;
