11ce2c76193bf88fdb77c93f6c66fbb4e7ed06b6
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-02-28 12:20:35 +0200

    renamed repositories in BE

--- src/app/api/test-db-connection/route.ts
+++ src/app/api/test-db-connection/route.ts
@@ -1,10 +1,10 @@
 import { createRoute } from '../../../server/shared/utils/create-route.util';
 import { NextResponse } from 'next/server';
-import { adminRepository } from '../../../server/entities/admin/infrastructure/admin.repository';
+import { adminApiRepository } from '../../../server/entities/admin/infrastructure/admin.repository';
 
 export const GET = createRoute({
   execute: async () => {
-    const res = await adminRepository.getItems(0, 50);
+    const res = await adminApiRepository.getItems(0, 50);
 
     console.log(res);
 
