31606e8fdaee1f55969c72d4ed88ae1446da519c
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-03-27 20:46:49 +0200

    feat(FIN-028): replaced bootstrap with shadcn ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,10 @@ import '@frontend/shared/styles/globals.scss';
 
 import dynamic from 'next/dynamic';
 import { ChildrenComponentProps } from '@frontend/shared/models/component-with-chilren.model';
+import { Inter } from 'next/font/google';
+import { cn } from '@frontend/ui/lib/utils';
+
+const inter = Inter({ subsets: ['latin'], variable: '--font-sans' });
 
 const ClientLayout = dynamic(() => import('./client-layout'), {
   ssr: false,
@@ -11,7 +15,10 @@ const ClientLayout = dynamic(() => import('./client-layout'), {
 
 export default function Layout({ children }: ChildrenComponentProps) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={cn('font-sans', inter.variable)}
+    >
       <body className="w-screen h-screen">
         <ClientLayout>{children}</ClientLayout>
       </body>
