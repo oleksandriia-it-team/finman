45056200b464b19ea303ca0aa1125c2bff719ffd
Mark Makarenko <lomak337@gmail.com>
2026-04-08 12:16:19 +0300

    fix(FIN-021): removed test-logout button

--- src/app/(profile)/layout.tsx
+++ src/app/(profile)/layout.tsx
@@ -3,12 +3,10 @@
 import { ChildrenComponentProps } from '@frontend/shared/models/component-with-chilren.model';
 import { UserMobileNavigationBar } from '@frontend/widgets/mobile-navigation/navigation-bar/navigation-bar';
 import { useUserGuard } from '@frontend/entities/profile/auth-guard.hook';
-import { useUserInformation } from '@frontend/shared/services/user-information/use-user-information.store';
 import { AuthorizedUserProvider } from '@frontend/shared/services/user-information/authorized-user.hook';
 
 export default function UserLayoutPage({ children }: ChildrenComponentProps) {
   const user = useUserGuard();
-  const logout = useUserInformation((s) => s.logOut);
 
   if (!user) {
     return null;
@@ -18,7 +16,6 @@ export default function UserLayoutPage({ children }: ChildrenComponentProps) {
     <AuthorizedUserProvider>
       <div className="size-full flex flex-col">
         <div className="flex-1">{children}</div>
-        <button onClick={logout}>Log out</button>
         <UserMobileNavigationBar />
       </div>
     </AuthorizedUserProvider>
