aa09b88364b888f7c6ee30c12a1f58bbb15e2f86
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-04-10 02:42:40 +0300

    feat(FIN-031): add pagination resource hook

---
+++ src/client/shared/hooks/pagination-resource/pagination-resource.hook.ts
@@ -0,0 +1,68 @@
+import { isEmpty } from '@common/utils/is-empty.util';
+import { useEffect, useMemo, useState } from 'react';
+import { PromiseState } from '@frontend/shared/enums/promise-state.enum';
+import { getErrorMessage } from '@common/utils/get-error-message.util';
+import {
+  PaginationResource,
+  PaginationResourceConfig,
+} from '@frontend/shared/hooks/pagination-resource/models/pagination-resource.model';
+import { useQuery } from '@tanstack/react-query';
+
+export function usePaginationResource<T, F extends object>({
+  getTotalCountFn,
+  getOptionsFn,
+  queryKey,
+  filters,
+}: PaginationResourceConfig<T, F>): PaginationResource<T> {
+  const [selectedPage, setPage] = useState<number>(1);
+
+  const getOptionsQuery = useQuery({
+    queryKey: [...queryKey, 'options', String(selectedPage), JSON.stringify(filters)],
+    queryFn: () => getOptionsFn(selectedPage, filters),
+    placeholderData: (previousData) => previousData,
+  });
+
+  const getTotalCountQuery = useQuery({
+    queryKey: [...queryKey, 'total-count', JSON.stringify(filters)],
+    queryFn: () => getTotalCountFn(filters),
+  });
+
+  useEffect(() => {
+    setPage(1);
+  }, [filters]);
+
+  const state = useMemo(() => {
+    const statuses = [getOptionsQuery.status, getTotalCountQuery.status];
+
+    if (statuses.every((status) => status === 'success')) {
+      return PromiseState.Success;
+    }
+
+    if (statuses.includes('error')) {
+      return PromiseState.Error;
+    }
+
+    return PromiseState.Loading;
+  }, [getOptionsQuery.status, getTotalCountQuery.status]);
+
+  const errorMessage = useMemo(() => {
+    const errors = [getOptionsQuery.error, getTotalCountQuery.error];
+
+    const error = errors.find((error) => !!error);
+
+    if (isEmpty(error)) {
+      return undefined;
+    }
+
+    return getErrorMessage(error);
+  }, [getOptionsQuery.error, getTotalCountQuery.error]);
+
+  return {
+    state,
+    options: getOptionsQuery.data ?? [],
+    totalCount: getTotalCountQuery.data ?? 0,
+    errorMessage,
+    selectedPage,
+    setPage,
+  };
+}
