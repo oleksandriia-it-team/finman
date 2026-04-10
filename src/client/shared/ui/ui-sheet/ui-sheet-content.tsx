0adfe0cbe8eed2f72d046890a3582b21d32afaf3
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-04-10 20:41:05 +0300

    feat(FIN-021): moved styles in scss in sheet and tooltip components

--- src/client/shared/ui/ui-sheet/ui-sheet-content.tsx
+++ src/client/shared/ui/ui-sheet/ui-sheet-content.tsx
@@ -8,6 +8,8 @@ import { UiSheetPortal } from './ui-sheet-portal';
 import { UiSheetOverlay } from './ui-sheet-overlay';
 import { UiSvgIcon } from '@frontend/ui/ui-svg-icon/ui-svg-icon';
 
+import './styles/ui-sheet-content-styles.scss';
+
 function UiSheetContent({
   className,
   children,
@@ -23,25 +25,19 @@ function UiSheetContent({
       <UiSheetOverlay />
       <SheetPrimitive.Content
         data-slot="sheet-content"
+        data-side={side}
         className={cn(
-          'fixed z-50 flex flex-col gap-4 bg-background shadow-lg transition ease-in-out data-[state=closed]:animate-out data-[state=closed]:duration-300 data-[state=open]:animate-in data-[state=open]:duration-500',
-          side === 'right' &&
-            'inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
-          side === 'left' &&
-            'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
-          side === 'top' &&
-            'inset-x-0 top-0 h-auto border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
-          side === 'bottom' &&
-            'inset-x-0 bottom-0 h-auto border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
+          'sheet-content',
           className,
         )}
         {...props}
       >
         {children}
         {showCloseButton && (
-          <SheetPrimitive.Close className="absolute top-4 right-4 rounded-xs opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:ring-2 focus:ring-ring focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <SheetPrimitive.Close className="sheet-close">
             <UiSvgIcon
               aria-hidden
+              size="default"
               name="x"
               className="pointer-events-none"
             />
--- src/client/shared/ui/ui-sheet/ui-sheet-overlay.tsx
+++ src/client/shared/ui/ui-sheet/ui-sheet-overlay.tsx
@@ -4,12 +4,14 @@ import * as React from 'react';
 import { Dialog as SheetPrimitive } from 'radix-ui';
 import { cn } from '@frontend/shared/utils/cn.util';
 
+import './styles/ui-sheet-overlay-styles.scss';
+
 function UiSheetOverlay({ className, ...props }: React.ComponentProps<typeof SheetPrimitive.Overlay>) {
   return (
     <SheetPrimitive.Overlay
       data-slot="sheet-overlay"
       className={cn(
-        'fixed inset-0 z-50 bg-black/50 data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:animate-in data-[state=open]:fade-in-0',
+        'sheet-overlay',
         className,
       )}
       {...props}
