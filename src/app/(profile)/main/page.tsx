bfee3763acc186c16b0ac63552de1090333f34d4
Mark Makarenko <lomak337@gmail.com>
2026-04-10 14:28:09 +0300

    feat(FIN-021): started to use pagination in regular-incomes-expenses-page.tsx

--- src/app/(profile)/main/page.tsx
+++ src/app/(profile)/main/page.tsx
@@ -1,20 +1,9 @@
 'use client';
 
 import { useAuthorizedUser } from '@frontend/shared/services/user-information/authorized-user.hook';
-import { IncomeExpenseCard } from '@frontend/entities/budget-plan/income-expense-card/income-expense-card';
-
+import RegularIncomesExpensesPage from '@frontend/features/regular-incomes-expenses/regular-incomes-expenses-page';
 export default function UserMainPage() {
   const user = useAuthorizedUser();
 
-  return (
-    <div className="size-full flex flex-col items-center p-5">
-      <IncomeExpenseCard
-        subtitle="Budget plan"
-        title="Expense"
-        type="expense"
-        amount="1000"
-        date={new Date()}
-      />
-    </div>
-  );
+  return <RegularIncomesExpensesPage />;
 }
--- src/client/features/regular-incomes-expenses/regular-incomes-expenses-page.tsx
+++ src/client/features/regular-incomes-expenses/regular-incomes-expenses-page.tsx
@@ -1,23 +1,49 @@
-import { myMocks } from '@frontend/features/regular-incomes-expenses/test-data';
 import { IncomeExpenseCard } from '@frontend/entities/budget-plan/income-expense-card/income-expense-card';
+import { MOCK_ALL_CATEGORIES } from './mock-card-data';
+import { usePaginationResource } from '@frontend/shared/hooks/pagination-resource/pagination-resource.hook';
+import { PromiseState } from '@frontend/shared/enums/promise-state.enum';
+import { FinPagination } from '@frontend/components/pagination/fin-pagination';
 
 export default function RegularIncomesExpensesPage() {
+  const pageSize = 5;
+  const { options, state, errorMessage, ...paginationRestProps } = usePaginationResource({
+    pageSize,
+    queryKey: ['test regular expenses and incomes'],
+    getOptionsFn: (page, pageSize) => {
+      const start = (page - 1) * pageSize,
+        end = start + pageSize;
+      return Promise.resolve(MOCK_ALL_CATEGORIES.slice(start, end));
+    },
+    getTotalCountFn: () => Promise.resolve(MOCK_ALL_CATEGORIES.length),
+  });
+
   return (
-    <div className="w-full flex flex-col pb-22 ">
+    <div className="w-full flex flex-col pb-8 ">
       <p className="text-xl p-4">
         <b>Регулярні доходи та витрати</b>
       </p>
-      <div className="flex flex-col items-center justify-center w-full flex-wrap ">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-2 p-4 ">
-          {myMocks.map((item, index) => (
-            <IncomeExpenseCard
-              key={index}
-              type={item.type}
-              title={item.title}
-              subtitle={item.subtitle}
-              amount={item.amount}
-            />
-          ))}
+      <div className="flex flex-col w-full flex-wrap ">
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4 ">
+          {state === PromiseState.Loading && 'Завантаження'}
+          {state === PromiseState.Error && (errorMessage || 'Помилка завантаження')}
+
+          {options.map((item) => {
+            return (
+              <IncomeExpenseCard
+                key={item.id}
+                category={item.category}
+                subtitle={item.subtitle}
+                type={item.type}
+                amount={item.amount}
+                date={item.date}
+              />
+            );
+          })}
+
+          <FinPagination
+            {...paginationRestProps}
+            pageSize={pageSize}
+          />
         </div>
       </div>
     </div>
