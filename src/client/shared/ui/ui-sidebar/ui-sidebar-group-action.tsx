bec285fd40082ba6e5cecdd3d3f1b57948f7da3d
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-04-11 22:43:51 +0300

    refactor(FIN-021): pr comments

--- src/client/shared/ui/ui-sidebar/ui-sidebar-group-action.tsx
+++ src/client/shared/ui/ui-sidebar/ui-sidebar-group-action.tsx
@@ -7,12 +7,14 @@ import './styles/sidebar-group-action-styles.scss';
 export function UiSidebarGroupAction({
   className,
   asChild = false,
+  type,
   ...props
 }: React.ComponentProps<'button'> & { asChild?: boolean }) {
   const Comp = asChild ? Slot.Root : 'button';
 
   return (
     <Comp
+      type={!asChild ? (type ?? 'button') : undefined}
       data-slot="sidebar-group-action"
       data-sidebar="group-action"
       className={cn('sidebar-group-action', className)}
