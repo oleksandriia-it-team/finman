1defe393100adb40c443cb8401152489701e5554
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-04-26 23:56:44 +0300

    feat(fin-036): replace UnregularEntry with MonthEntry in budget plan records and services

--- src/client/entities/month-entry/month-entry.local.repository.ts
+++ src/client/entities/month-entry/month-entry.local.repository.ts
@@ -1,19 +1,19 @@
 import { CrudLocalService } from '@frontend/database/crud/crud.local.service';
-import type { UnregularEntry } from '@common/records/unregular-entry.record';
+import type { MonthEntry } from '@common/records/month-entry.record';
 import { databaseLocalService, type DatabaseLocalService } from '@frontend/database/database.local.service';
 import { Tables } from '@frontend/shared/constants/database.constants';
 import type { DefaultColumnKeys } from '@common/models/default-table-columns.model';
 
-export class UnregularEntryLocalRepository extends CrudLocalService<UnregularEntry, never> {
+export class MonthEntryLocalRepository extends CrudLocalService<MonthEntry, never> {
   constructor(databaseLocalService: DatabaseLocalService) {
     super(databaseLocalService, Tables.UnregularEntries);
   }
 
-  createItem(data: Omit<UnregularEntry, DefaultColumnKeys>): Promise<number> {
+  createItem(data: Omit<MonthEntry, DefaultColumnKeys>): Promise<number> {
     return this.databaseLocalService.updateOrCreateItem(this.tableName, data);
   }
 
-  updateItem(id: number, data: Omit<UnregularEntry, DefaultColumnKeys>): Promise<true> {
+  updateItem(id: number, data: Omit<MonthEntry, DefaultColumnKeys>): Promise<true> {
     return this.databaseLocalService.updateOrCreateItem(this.tableName, { id, ...data }).then(() => true as const);
   }
 
@@ -22,4 +22,4 @@ export class UnregularEntryLocalRepository extends CrudLocalService<UnregularEnt
   }
 }
 
-export const unregularEntryLocalRepository = new UnregularEntryLocalRepository(databaseLocalService);
+export const monthEntryLocalRepository = new MonthEntryLocalRepository(databaseLocalService);
