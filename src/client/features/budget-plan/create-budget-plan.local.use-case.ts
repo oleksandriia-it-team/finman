470ed112d97aef454e0da2f9e056febf11f05259
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-04-29 00:47:14 +0300

    feat(FIN-036): add local use cases for creating and updating budget plans

---
+++ src/client/features/budget-plan/create-budget-plan.local.use-case.ts
@@ -0,0 +1,26 @@
+import type { BudgetPlan, BudgetPlanDto } from '@common/records/budget-plan.record';
+import { type ITransactionManager, TransactionalUseCase } from '@common/models/transaction-manager.model';
+import type { ICrudService } from '@common/models/crud-service.model';
+import type { MonthEntry } from '@common/records/month-entry.record';
+
+export class CreateBudgetPlanLocalUseCase extends TransactionalUseCase<BudgetPlanDto, number> {
+  constructor(
+    transactionManager: ITransactionManager,
+    private budgetPlanRepository: ICrudService<BudgetPlan>,
+    private monthEntryRepository: ICrudService<MonthEntry>,
+  ) {
+    super(transactionManager);
+  }
+
+  async handle({ otherEntries: otherEntriesDto, ...data }: BudgetPlanDto): Promise<number> {
+    const id = await this.budgetPlanRepository.createItem({ ...data, otherEntryIds: [] });
+
+    const otherEntryIds = await Promise.all(
+      otherEntriesDto.map((dto) => this.monthEntryRepository.createItem({ ...dto, budgetPlanId: id })),
+    );
+
+    await this.budgetPlanRepository.updateItem(id, { ...data, otherEntryIds });
+
+    return id;
+  }
+}
