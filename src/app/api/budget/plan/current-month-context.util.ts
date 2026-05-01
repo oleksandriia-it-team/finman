bf0a1b62996638b7662cfaa4322ada3c57683478
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-05-01 19:20:09 +0300

    feat(FIN-036): enhance budget plan functionality with user-specific context and error handling

--- src/app/api/budget/plan/current-month-context.util.ts
+++ src/app/api/budget/plan/current-month-context.util.ts
@@ -2,13 +2,14 @@ import type { BudgetPlanContext } from './current-month-context.model';
 import { GetUserIdTransformer } from '@backend/shared/transformers/get-user-id.transformer';
 import { getCurrentMonthDate } from '@common/domains/budget-plan/get-current-month-date-util';
 import { budgetPlanRepository } from '@backend/entities/budget-plan/infrastructure/budget-plan.repository';
+import { isEmpty } from '@common/utils/is-empty.util';
 
-export function getCurrentMonthBudgetPlanContext(request: Request): Promise<BudgetPlanContext> {
+export async function getCurrentMonthBudgetPlanContext(request: Request): Promise<BudgetPlanContext> {
   const userId = await GetUserIdTransformer(request);
 
   const date = getCurrentMonthDate();
 
-  if (!userId) {
+  if (isEmpty(userId)) {
     return {
       ...date,
       userId,
