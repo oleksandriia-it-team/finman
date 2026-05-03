8a305cf846131afaeb19639b231fe2a1e8961dab
IeuanZorander <reapersansaut@gmail.com>
2026-05-03 14:15:48 +0300

    refactor(recovery): simplify UserExistsRecoveryGuard implementation and improve email handling

--- src/server/entities/recovery-code/infrastructure/user-exist.guard.ts
+++ src/server/entities/recovery-code/infrastructure/user-exist.guard.ts
@@ -1,13 +1,22 @@
 import type { ForgotPasswordDto } from '@common/domains/auth/schema/forgot-password.schema';
 import { userApiRepository } from '@backend/entities/user/infrastructure/user.repository';
-import type { ApiResultOperationError } from '@common/models/api-result-operation.model';
+import type { ApiResultOperation } from '@common/models/api-result-operation.model';
 
-export async function UserExistsRecoveryGuard({ body }: { body: ForgotPasswordDto }) {
-  const user = await userApiRepository.repository.findOneBy({
-    email: body.email as string,
-  });
+export async function UserExistsRecoveryGuard({
+  body,
+}: {
+  body: ForgotPasswordDto;
+}): Promise<ApiResultOperation<boolean> | null> {
+  const email = body.email.trim().toLowerCase();
+
+  const user = await userApiRepository.repository.findOneBy({ email });
+
   if (!user) {
-    return { status: 200, data: true } as unknown as ApiResultOperationError;
+    return {
+      status: 200,
+      data: true,
+    };
   }
+
   return null;
 }
