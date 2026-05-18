5768ebb5e43109ed69e8a6a7d72ec9d90693d959
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-05-18 15:00:09 +0300

    feat(FIN-002): enhance layout component to parse budget plan date from URL and update state accordingly

--- src/app/(profile)/profile/budget/budget-plan/(budget-plan)/[id]/layout.tsx
+++ src/app/(profile)/profile/budget/budget-plan/(budget-plan)/[id]/layout.tsx
@@ -1,10 +1,11 @@
 'use client';
 
-import { useEffect } from 'react';
+import { useEffect, useMemo } from 'react';
 import { usePathname, useRouter } from 'next/navigation';
 import { createBudgetPlanIdUrl } from '@common/domains/budget-plan/create-budget-plan-param-url.util';
 import type { ChildrenComponentProps } from '@frontend/shared/models/component-with-chilren.model';
 import { useSelectedBudgetPlan } from '@frontend/features/budget-plan/hooks/selected-budget-plan.hook';
+import { parseBudgetPlanDateParam } from '@common/domains/budget-plan/get-budget-plan-date-param.util';
 
 const IdIndexInPath = 2;
 
@@ -24,6 +25,34 @@ export default function BudgetPlanIdLayout({ children }: ChildrenComponentProps)
       router.replace(segments.join('/'));
     }
   }, [pathname, router, selectedBudgetPlanDate]);
+
+  const routeDate = useMemo(() => {
+    const segments = pathname.split('/');
+    const id = segments[IdIndexInPath];
+
+    const date = parseBudgetPlanDateParam(id);
+
+    if (date.success) {
+      return date.data;
+    }
+
+    return selectedBudgetPlanDate;
+  }, [pathname, selectedBudgetPlanDate]);
+
+  useEffect(() => {
+    if (routeDate.month === selectedBudgetPlanDate.month && routeDate.year === selectedBudgetPlanDate.year) {
+      return;
+    }
+
+    setBudgetPlanDate(routeDate);
+  }, [
+    routeDate,
+    routeDate.month,
+    routeDate.year,
+    selectedBudgetPlanDate.month,
+    selectedBudgetPlanDate.year,
+    setBudgetPlanDate,
+  ]);
 
   return children;
 }
