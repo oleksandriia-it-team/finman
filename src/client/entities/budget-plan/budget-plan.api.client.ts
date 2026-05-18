3f30e91f98208d6b429ed6c2eacf233f65353a6a
Mark Makarenko <lomak337@gmail.com>
2026-05-18 12:22:57 +0300

    fix(fin-056): implement IBudgetPlanRepository in BudgetPlanApiClient

--- src/client/entities/budget-plan/budget-plan.api.client.ts
+++ src/client/entities/budget-plan/budget-plan.api.client.ts
@@ -3,8 +3,9 @@ import type { GetBudgetPlanDto } from '@common/domains/budget-plan/get-budget-pl
 import { fetchClient } from '@frontend/shared/services/fetch-client/fetch-client.service';
 import type { ApiResultOperationSuccess } from '@common/models/api-result-operation.model';
 import type { CreateBudgetPlanDto, UpdateBudgetPlanDto } from '@common/domains/budget-plan/budget-plan.schema';
+import type { IBudgetPlanRepository } from '@common/domains/budget-plan/budget-plan.repository.model';
 
-export class BudgetPlanApiClient {
+export class BudgetPlanApiClient implements IBudgetPlanRepository {
   createItem(input: CreateBudgetPlanDto): Promise<number> {
     return fetchClient
       .post<ApiResultOperationSuccess<number>>('/api/budget/plan/create-current-month', input)
