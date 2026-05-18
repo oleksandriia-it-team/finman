d9900e097c9c8e58f675c611effefd8068789bb1
Mark Makarenko <lomak337@gmail.com>
2026-05-18 13:15:58 +0300

    fix(fin-056): refactor input group component to use div and update styles

--- src/client/shared/ui/ui-input-group/ui-input-group.tsx
+++ src/client/shared/ui/ui-input-group/ui-input-group.tsx
@@ -5,10 +5,11 @@ import { cn } from '@frontend/shared/utils/cn.util';
 
 import './styles/input-group-styles.scss';
 
-export function UiInputGroup({ className, ...props }: React.ComponentProps<'fieldset'>) {
+export function UiInputGroup({ className, ...props }: React.ComponentProps<'div'>) {
   return (
-    <fieldset
+    <div
       data-slot="input-group"
+      role="group"
       className={cn('input-group basic-input group/input-group', className)}
       {...props}
     />
