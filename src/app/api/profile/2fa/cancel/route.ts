fdbe40e1002c0ca1085532ae3a606d016465e69b
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-05-23 02:51:07 +0300

    feat(FIN-048): add route for deleting TOTP items with authentication guards

---
+++ src/app/api/profile/2fa/cancel/route.ts
@@ -0,0 +1,16 @@
+import { createRoute } from '@backend/shared/utils/create-route.util';
+import { getTwoFactorSetupContext } from '../2fa-setup-context.util';
+import { AuthGuard } from '@backend/entities/user/infrastructure/auth.guard';
+import { TotpEnabledGuard } from '@backend/features/totp/totp-enabled.guard';
+import type { TotpOrm } from '@backend/entities/totp/infrastructure/totp.orm';
+import { totpRepository } from '@backend/entities/totp/infrastructure/totp.repository';
+
+export const POST = createRoute({
+  contextFn: getTwoFactorSetupContext,
+  guards: [AuthGuard, ({ context: { totp } }) => TotpEnabledGuard(totp)],
+  execute: async ({ context }) => {
+    const totpEntity = context.totp as TotpOrm;
+
+    return { status: 200, data: await totpRepository.deleteItem(totpEntity.id) };
+  },
+});
