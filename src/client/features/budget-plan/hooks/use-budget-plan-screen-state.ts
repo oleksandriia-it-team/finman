1bb1cffe4035ce6d049df113d3803af07404d6e6
Mark Makarenko <lomak337@gmail.com>
2026-06-13 13:48:41 +0300

    fix(fin-058): removed pagination from budget-plan-create-screen.tsx, and changed to TTQ

--- src/client/features/budget-plan/hooks/use-budget-plan-screen-state.ts
+++ src/client/features/budget-plan/hooks/use-budget-plan-screen-state.ts
@@ -1,40 +1,25 @@
 import type { BudgetPlanFormScreenProps } from '@frontend/features/budget-plan/budget-plan-form/create-budget-plan/budget-plan-create-screen-props';
-import { calculateFromAndTo } from '@common/utils/calculate-from-and-to.util';
 import { useRegularTransactions } from '@frontend/features/regular-incomes-expenses/card-creation-form/regular-transaction.hook';
-import { usePaginationResource } from '@frontend/shared/hooks/pagination-resource/pagination-resource.hook';
+import { useQuery } from '@tanstack/react-query';
+import { getPromiseState } from '@frontend/shared/utils/get-promise-state.util';
+import { getFirstAppError } from '@common/utils/get-first-app-error.util';
+
+const MaxRegularTransactions = 1000;
 
 export function useBudgetPlanScreenState({ initialData, onCancel }: BudgetPlanFormScreenProps) {
   const isEdit = !!initialData;
+  const { getPayments } = useRegularTransactions();
 
-  const pageSize = 5;
-  const { getPayments, getTotalCount } = useRegularTransactions();
-
-  const {
-    options,
-    state: listState,
-    appError,
-    ...paginationRestProps
-  } = usePaginationResource({
-    pageSize,
+  const { data, status, error } = useQuery({
     queryKey: ['budget-plan-regular-transactions'],
-    getOptionsFn: async (page, pageSize) => {
-      const { from, to } = calculateFromAndTo(page, pageSize);
-      return getPayments(from, to);
-    },
-    getTotalCountFn: async () => {
-      const count = await getTotalCount();
-      return count ?? 0;
-    },
-    clearCacheOnDestroy: true,
+    queryFn: () => getPayments(1, MaxRegularTransactions),
+    staleTime: 0,
   });
 
   return {
-    state: listState,
-    listState,
-    options,
-    appError,
-    paginationRestProps,
-    pageSize,
+    state: getPromiseState(status),
+    options: data ?? [],
+    appError: getFirstAppError(error),
     isEdit,
     onCancel,
   };
