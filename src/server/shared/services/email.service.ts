12bbdd3f5aec38cf6bf61921356d4006862f9d1d
Dmytro Reshetylo <xandaychannel@gmail.com>
2026-06-15 22:30:52 +0300

    feat: fix send mail transport

--- src/server/shared/services/email.service.ts
+++ src/server/shared/services/email.service.ts
@@ -1,18 +1,20 @@
 import { createTransport } from 'nodemailer';
 import { EnvConfigConstant } from '@backend/config/env-config.constant';
+import type SMTPTransport from 'nodemailer/lib/smtp-transport';
 
 const transporter = createTransport({
-  service: 'gmail',
+  host: 'smtp.gmail.com',
   secure: true,
   port: 465,
   auth: {
     user: EnvConfigConstant.GMAIL_EMAIL,
     pass: EnvConfigConstant.GMAIL_APP_PASSWORD,
   },
+  family: 4,
   connectionTimeout: 5000,
   greetingTimeout: 5000,
   socketTimeout: 10000,
-});
+} as SMTPTransport.Options);
 
 export function sendEmail(to: string, subject: string, html: string) {
   return transporter.sendMail({ from: '"FinMan" <' + EnvConfigConstant.GMAIL_EMAIL + '>', to, subject, html });
